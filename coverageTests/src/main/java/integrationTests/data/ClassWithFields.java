e17213b4c39c87d0930699c1990590a4cd189683
Jeremy Landis <jeremylandis@hotmail.com>
2023-06-18 16:16:56 -0400

    [fix] Add back and add missing elements to classWithFields

--- coverageTests/src/main/java/integrationTests/data/ClassWithFields.java
+++ coverageTests/src/main/java/integrationTests/data/ClassWithFields.java
@@ -2,11 +2,13 @@ package integrationTests.data;
 
 public class ClassWithFields {
     private static int static1;
+    private static String static2 = "B2";
     private static long static3;
 
     // Instance fields:
     // (coverage accounts for each owner instance)
     private boolean instance1 = true;
+    private Boolean instance2;
     private double instance3;
 
     public static int getStatic1() {
@@ -15,6 +17,14 @@ public class ClassWithFields {
 
     public static void setStatic1(int static1) {
         ClassWithFields.static1 = static1;
+    }
+
+    public static String getStatic2() {
+        return static2;
+    }
+
+    public static void setStatic2(String static2) {
+        ClassWithFields.static2 = static2;
     }
 
     public static long getStatic3() {
@@ -36,6 +46,14 @@ public class ClassWithFields {
 
     public void setInstance1(boolean instance1) {
         this.instance1 = instance1;
+    }
+
+    public Boolean getInstance2() {
+        return instance2;
+    }
+
+    public void setInstance2(Boolean instance2) {
+        this.instance2 = instance2;
     }
 
     public double getInstance3() {
