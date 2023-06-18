ce3b7e7a1a24cd53a7a00fcbf0f9a325f57e5606
Jeremy Landis <jeremylandis@hotmail.com>
2023-06-18 16:42:34 -0400

    [fix] Correct tests now that lines changed

--- coverageTests/src/test/java/integrationTests/UnreachableStatementsTest.java
+++ coverageTests/src/test/java/integrationTests/UnreachableStatementsTest.java
@@ -30,12 +30,12 @@ public final class UnreachableStatementsTest extends CoverageTest {
     public void branchingMethodWithUnreachableLines_avoidAssertion() {
         tested.branchingMethodWithUnreachableLines(0);
 
-        assertLines(14, 20, 3);
-        assertLine(14, 1, 1, 1);
-        assertLine(15, 1, 0, 0);
-        assertLine(16, 1, 0, 0);
-        assertLine(19, 1, 1, 1);
-        assertLine(20, 1, 1, 1);
+        assertLines(23, 29, 3);
+        assertLine(23, 1, 1, 1);
+        assertLine(24, 1, 0, 0);
+        assertLine(25, 1, 0, 0);
+        assertLine(28, 1, 1, 1);
+        assertLine(29, 1, 1, 1);
     }
 
     @Test
@@ -46,9 +46,9 @@ public final class UnreachableStatementsTest extends CoverageTest {
         }
 
         // Accounts for executions from previous test.
-        assertLines(14, 20, 4);
-        assertLine(14, 1, 1, 2);
-        assertLine(15, 1, 1, 1);
-        assertLine(16, 1, 0, 0);
+        assertLines(23, 29, 4);
+        assertLine(23, 1, 1, 2);
+        assertLine(24, 1, 1, 1);
+        assertLine(25, 1, 0, 0);
     }
 }
